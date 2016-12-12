3088daed56d967164b43c86dfbb7b5a749b1e215
Steven A. Villegas C <savillegas@open.com.co>
2016-12-12 06:51:53 -0500

    removed all transpiled files

--- client/app/home/login/login.component.ts
+++ client/app/home/login/login.component.ts
@@ -12,50 +12,20 @@ import { AuthService } from '../auth/auth.service';
   selector: 'login-form',
   templateUrl: 'login.component.html',
   styleUrls: ['login.component.css'],
-  providers: [AuthService]			 
+  providers: [AuthService]
 })
 
-export class LoginComponent implements OnInit { 
+export class LoginComponent {
 
-	heroes: Hero[];
+	title = 'Login Component';
 
-	selectedHero: Hero;
-
-	title = 'Hero Editor';
+  loggedUser: User;
 
 	constructor(
     private router: Router,
     private authService: AuthService) { }
 
-	ngOnInit(): void {
-		this.getHeroes();
-  }
-
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
-  	
-	onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-  gotoDetail(): void {
-    this.router.navigate(['/detail', this.selectedHero.id]);
-  }
-
-  add(name: string): void {
-    name = name.trim();
-    if (!name) { return; }
-    this.heroService.create(name)
-          .then(hero => {this.heroes.push(hero);
-            this.selectedHero = null;});
-  }
-
-  delete(hero: Hero): void {
-    this.heroService.delete(hero.id)
-      .then(() => {
-        this.heroes = this.heroes.filter(h => h !== hero);
-        if (this.selectedHero === hero) { this.selectedHero = null; }
-      });
+  login(username:string, password:string): void{
+    this.authService.login(username, password).then(loggedUser => this.loggedUser = loggedUser);
   }
 }
