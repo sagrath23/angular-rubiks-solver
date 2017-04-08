0f88e84d49df03cc8efbafcdbc7632be0be9c17f
Steven Villegas <sagrath23@gmail.com>
2017-04-07 21:14:09 -0500

    Updated

--- client/app/home/tracker/tracker.component.js
+++ client/app/home/tracker/tracker.component.js
@@ -26,7 +26,7 @@ var TrackerComponent = (function () {
     }
     TrackerComponent.prototype.ngOnInit = function () {
         tracking.ColorTracker.registerColor('blue', function (r, g, b) {
-            if (r < 120 && g < 160 && b > 100) {
+            if (r < 140 && g < 190 && b > 80) {
                 return true;
             }
             return false;
